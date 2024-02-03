8053b7112c595f2a8b3744120cd0fc370e0dd275
purpella <ddaniluk72@gmail.com>
2024-02-03 17:24:37 +0300

    Future-prediction bot

---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from random import choices
+
+BOT_TOKEN = '6865223416:AAGJb7vGYnXkIQrKr744H_8jhy6F34SUm_M'
+
+prediction = ['Насколько я понимаю, да',
+              'Спросите еще раз позже.',
+              'Лучше не говорить вам сейчас.',
+              'Сейчас не могу предсказать.',
+              'Сосредоточься и спроси еще раз.',
+              'Не рассчитывайте на это.',
+              'Это точно.',
+              'Это определенно так.',
+              'Скорее всего.',
+              'Мой ответ - нет.',
+              'Мои источники говорят, что нет.',
+              'Перспективы не очень хорошие.',
+              'Перспективы хорошие.',
+              'Ответить туманно, попробуйте еще раз.',
+              'Знаки указывают на "да".',
+              'Очень сомнительно.',
+              'Без сомнения.',
+              'ДА.',
+              'Да, определенно.',
+              'Вы можете положиться на это.']
+
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Purpl-bot\n Спроси у меня что-нибудь!')
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе предсказание '
+        'на твое будущее'
+    )
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=random.choice(prediction))
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
